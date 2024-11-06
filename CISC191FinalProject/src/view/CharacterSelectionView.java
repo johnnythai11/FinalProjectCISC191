4b7824669dba1c450078d72af8397e9214a56dd8
johnny200138 <jthai004@student.sdccd.edu>
2024-11-06 10:05:19 -0800

    change

--- CISC191FinalProject/src/view/CharacterSelectionView.java
+++ CISC191FinalProject/src/view/CharacterSelectionView.java
@@ -17,12 +17,65 @@ package view;
  * 
  */
 
-public class CharacterSelectionView
+import java.awt.*;
+import javax.swing.*;
+public class CharacterSelectionView extends JFrame
 {
+final int SCREEN_WIDTH = 650, SCREEN_HEIGHT = 800;
+private JLabel characterIcon,statField;
+private JButton mage,tank,melee,confirmButton;
 
-	public CharacterSelectionView()
-	{
-		// TODO Auto-generated constructor stub
-	}
+CharacterSelectionView()
+{
+super("Select Your Fighter");
+super.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+setSize(SCREEN_WIDTH,SCREEN_HEIGHT);
+setResizable(false);
+super.setLayout(null);
 
+setElements();
+
+setVisible(true);
+}
+
+private void setElements()
+{
+characterIcon = new JLabel();
+mage = new JButton("Mage");
+tank = new JButton("Tank");
+melee = new JButton("Melee");
+confirmButton = new JButton("Confirm");
+statField = new JLabel();
+
+
+statField.setText("Loras ipsum dolar set amri, i speak latin so good");
+statField.setOpaque(true);
+statField.setBackground(Color.RED);
+characterIcon.setBackground(Color.black);
+characterIcon.setOpaque(true);
+
+
+characterIcon.setBounds(25, 25, 250, 250);
+mage.setBounds(25,290,250,50);
+tank.setBounds(25,355,250,50);
+melee.setBounds(25,420,250,50);
+confirmButton.setBounds(350,290,250,50);
+statField.setBounds(350, 25, 250, 250);
+
+add(mage);
+add(tank);
+add(melee);
+add(confirmButton);
+add(statField);
+add(characterIcon);
+}
+
+
+
+
+
+public static void main(String[] args)
+{
+new CharacterSelectionView();
+}
 }
