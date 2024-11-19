0fda8c238266e3ec3911778658ba38243ef63896
johnny200138 <jthai004@student.sdccd.edu>
2024-11-18 21:02:55 -0800

    change

--- src/view/MainMenuView.java
+++ src/view/MainMenuView.java
@@ -1,10 +1,3 @@
-package view;
-
-import javax.swing.JButton;
-import javax.swing.JFrame;
-
-import model.SaveLoad;
-
 /**
  * Lead Author(s):
  * @author Full name: Johnny Thai
@@ -21,19 +14,20 @@ import model.SaveLoad;
  * Version: 2024-10-16
  * 
  */
-
-import java.awt.*;
+package view;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import model.SaveLoad;
 import javax.swing.*;
+
 public class MainMenuView extends JFrame {
 
 	private JButton howToPlayButton;
 	private JButton charecterSelectScreenButton;
 	private JButton loadButton;
 	private JPanel mainPanel;
-
 	final int SCREEN_WIDTH = 750, SCREEN_HEIGHT = 800;
 
 
@@ -67,16 +61,14 @@ public class MainMenuView extends JFrame {
 		charecterSelectScreenButton = new JButton("Charecter Selection");
 		howToPlayButton = new JButton("How To Play");
 		loadButton = new JButton("Load Game");
-		
-		
+
 		charecterSelectScreenButton.addActionListener(new CharacterSelectListener());
 		howToPlayButton.addActionListener(new HowToPlayListener());
 		loadButton.addActionListener(new LoadListener());
-		
+
 		charecterSelectScreenButton.setBounds(offsetLength,offsetHeightChar,BUTTON_WIDTH,BUTTON_HEIGHT);
 		howToPlayButton.setBounds(offsetLength, offsetHeightHow, BUTTON_WIDTH, BUTTON_HEIGHT);
 		loadButton.setBounds(offsetLength, offsetHeightLoad, BUTTON_WIDTH, BUTTON_HEIGHT);
-
 
 		mainPanel.add(charecterSelectScreenButton);
 		mainPanel.add(howToPlayButton);
@@ -86,40 +78,35 @@ public class MainMenuView extends JFrame {
 	public static void main(String[] args)
 	{
 		new MainMenuView();
-
 	}
-	
+
 	private class CharacterSelectListener implements ActionListener
 	{
-
 		@Override
 		public void actionPerformed(ActionEvent e)
 		{
 			new CharacterSelectionView();
 			dispose();
 		}
-		
 	}
-	
+
 	private class HowToPlayListener implements ActionListener
 	{
-
 		@Override
 		public void actionPerformed(ActionEvent e)
 		{
 			new HowToPlayScreenView();
 			dispose();
 		}
-		
+
 	}
+	
 	private class LoadListener implements ActionListener
 	{
-
 		@Override
 		public void actionPerformed(ActionEvent e)
 		{
 			SaveLoad.binaryLoad();
-			
 		}
 	}
 }
